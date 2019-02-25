210bdb4ab5b9434773430f668c4affb122d25909
Joshua Lewis <joshlewis@gmail.com>
2019-02-25 10:03:43 -0500

    changed currency to individual

--- app/code/local/BitpayCheckout/BPCheckout/Crypto.php
+++ app/code/local/BitpayCheckout/BPCheckout/Crypto.php
@@ -4,8 +4,8 @@ class BitpayCheckout_BPCheckout_Crypto
     public function toOptionArray()
     {
         return array(
-	        array('value' => 'BTC', 'label'=>Mage::helper('adminhtml')->__('BTC')),
-	        array('value' => 'BCH', 'label'=>Mage::helper('adminhtml')->__('BCH'))
+	        array('name'=>'bitpay_crypto[]','value' => 'BTC', 'label'=>Mage::helper('adminhtml')->__('BTC')),
+	        array('name'=>'bitpay_crypto[]','value' => 'BCH', 'label'=>Mage::helper('adminhtml')->__('BCH'))
         );
     }
 }
